91fc644fe7f5503ac57827b5ad27b9cff80339da
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2020-03-26 11:49:27 -0500

    domain fixed

--- brein/general/utils.ts
+++ brein/general/utils.ts
@@ -4,7 +4,7 @@ const deleteCookie = async (name: string) => {
     // document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=minsky.cc";
     try {
         console.log("login out with brauth by next api");
-        const r = await axios.get("http://127.0.0.1:3000/api/logout");
+        const r = await axios.get("https://brein.minsky.cc/api/logout");
         console.log(r);
     } catch (err) {
         console.log(err);
