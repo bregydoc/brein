4014f96c3d517472faefd667530f6dbd6269710f
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2020-03-26 10:48:31 -0500

    testing login out

--- brein/general/utils.ts
+++ brein/general/utils.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 const deleteCookie = async (name: string) => {
     // document.cookie = name + "=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
     try {
+        console.log("login out with brauth");
         const r = await axios.get("https://bauth.minsky.cc/login?logout=true");
         console.log(r);
     } catch (err) {
