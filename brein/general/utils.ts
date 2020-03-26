93f18c5af67753f04411af8bc9009cfd623016ec
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2020-03-26 11:03:49 -0500

    deleting cookie by cliend and server

--- brein/general/utils.ts
+++ brein/general/utils.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const deleteCookie = async (name: string) => {
-    // document.cookie = name + "=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+    document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+
     try {
         console.log("login out with brauth");
-        const r = await axios.get("https://brauth.minsky.cc/login?logout=true");
+        const r = await axios.post("https://brauth.minsky.cc/login?logout=true");
         console.log(r);
     } catch (err) {
         console.log(err);
