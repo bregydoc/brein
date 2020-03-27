2a50105108e935e3cc14a95fc762d688da49ec57
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2020-03-27 15:20:48 -0500

    login rewrite

--- brein/pages/api/login.ts
+++ brein/pages/api/login.ts
@@ -1,14 +1,14 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
     console.log("login with brauth");
     if (req.body) {
         const username = req.body.username;
         const password = req.body.username;
 
-        axios
-            .post(
+        try {
+            const r = await axios.post(
                 "https://brauth.minsky.cc/login",
                 {
                     username: username,
@@ -19,18 +19,17 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
                         "Content-Type": "application/json"
                     }
                 }
-            )
-            .then(r => {
-                console.log(r.headers);
-                for (let header in r.headers) {
-                    const val = r.headers[header];
-                    res.setHeader(header, val);
-                }
-                res.send(r.data);
-                res.status(r.status).end();
-            })
-            .catch(err => {
-                console.log(err);
-            });
+            );
+
+            console.log(r.headers);
+            for (let header in r.headers) {
+                const val = r.headers[header];
+                res.setHeader(header, val);
+            }
+            res.send(r.data);
+            res.status(r.status).end();
+        } catch (err) {
+            console.log(err);
+        }
     }
 };
