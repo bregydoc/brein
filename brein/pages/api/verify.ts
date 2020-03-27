37c20362fc63dd433977e640161a5487a62edfb7
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2020-03-27 11:28:23 -0500

    register new log netry

--- brein/pages/api/verify.ts
+++ brein/pages/api/verify.ts
@@ -4,6 +4,7 @@ import jwt from "jsonwebtoken";
 export default (req: NextApiRequest, res: NextApiResponse) => {
     const token: string = req.cookies["jwt_token"];
 
+    console.log("verifing token");
     const secret = process.env.JWT_SECRET || "";
     jwt.verify(token, secret, (err: any, decoded: any) => {
         if (err) {
@@ -11,6 +12,7 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
             res.status(403).json({ cause: "cannot perform token verification" });
             return;
         }
+        console.log(decoded);
         res.status(200).json({ claims: decoded });
     });
 };
